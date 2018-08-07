e060e20aaad9a914998d065cc3665298b8ce559f
shomychen <shomychen@sina.com>
2018-08-07 15:39:22 +0800

    使用GET连接测试本地数据库

--- src/controller/test.js
+++ src/controller/test.js
@@ -1,16 +1,19 @@
 module.exports = class extends think.Controller {
-	indexAction () {
-		this.body = 'hello test index!';
-	}
-	getAction () {
-		this.body = 'hello test get!';
-	}
-	infoAction() {
-		this.body = 'hello test info!';
-	}
-	nameAction() {
-		name = this.post('id');
-		// this.body = name;
-		console.log(name);
-	}
+  indexAction() {
+    this.body = 'hello test index!';
+  }
+
+  getAction() {
+    this.body = 'hello test get!';
+  }
+
+  infoAction() {
+    this.body = 'hello test info!';
+  }
+
+  nameAction() {
+    const name = this.post('id');
+    // this.body = name;
+    console.log(name);
+  }
 };
--- src/controller/user.js
+++ src/controller/user.js
@@ -2,9 +2,45 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
   indexAction() {
-    return 3;
+    return this.json({
+      status: 200,
+      data: '用户名'
+    });
   }
-  getList() {
-    return this.field('name').select();
+
+  async getAction() {
+    const user = this.model('user'); // controller 里实例化模型
+    const data = await user.select(); // 多条查询select()
+    return this.success(data);
   }
+
+  // 查看
+  async queryAction() {
+    const username = this.get('username'); // 传值是否传username测试用：http://localhost:8361/user/userName?username=admin，
+    console.log(username);
+    const model = this.model('user'); // SELECT 用户表，因配置加了prefix所以是里查找到的表是'demo_user'
+    const data = await model.where({
+      'username': username
+    }).find(); // 单条查询find()表中是否包含get传参过来的username
+    let resultData = {};
+    let msg = '';
+    // 判断是否已经存在于数据中
+    if (think.isEmpty(data.username)) {
+      resultData = {
+        exist: false
+      };
+      msg = '用户名不存在';
+    } else {
+      resultData = {
+        exist: true
+      };
+      msg = '用户名存在';
+    }
+    return this.json({
+      status: 200,
+      data: resultData || {},
+      msg: msg,
+    });
+  }
+
 };
